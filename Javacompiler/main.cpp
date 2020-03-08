37e9a7da3a9a7c07099e1d54419f3b64b00884d4
Zhang Enzhi <zhangsuiyu657@gmail.com>
2020-03-08 10:59:41 +0800

    Initial Commit

---
+++ Javacompiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Javacompiler
+//
+//  Created by 张恩智 on 2020/03/08.
+//  Copyright © 2020 张恩智. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
